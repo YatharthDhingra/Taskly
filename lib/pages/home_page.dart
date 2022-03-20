40ebe0ba2125cf9ab6bfd03080dfddee47b9b698
Yatharth Dhingra <yatharthdhingra1@gmail.com>
2022-03-20 19:12:41 +0530

    added a circular progress indicator

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
 //because the state of this widget will change in our app, so stateful
 class HomePage extends StatefulWidget {
@@ -18,6 +19,8 @@ class _HomePageState extends State<HomePage> {
 
   late double _deviceHeight, _deviceWidth;
 
+  String? _newTaskContent; //for what we input in textfield
+
   _HomePageState(); //empty constructor
   @override
   Widget build(BuildContext context) {
@@ -32,8 +35,35 @@ class _HomePageState extends State<HomePage> {
           style: TextStyle(fontSize: 25),
         ), //to set the title
       ), //top bar of the page
-      body: _tasksList(), //set the body to list View
-      floatingActionButton: _addTaskButton(),  //to add a floatingActionButton on our scaffold
+      body: _tasksView(), //set the body to list View
+      floatingActionButton:
+          _addTaskButton(), //to add a floatingActionButton on our scaffold
+    );
+  }
+
+//This function can't be async as UI can't be async -> something needs to be rendered on the screen
+  Widget _tasksView() {
+    //creating a box in hive with a  name , a box in hive is a container that stores data
+    // Hive.openBox("tasks");  ---> it returns a box (this is a future , so we need to wait for it to complete , then only we can re-render the list on the screen)
+    //but since UI can't be async , we can't use await here
+
+    //so we use FutureBuilder method
+    //it takes in a future and a builder function(that defines when this future has been resolved or when it hasn't been resolved)
+    return FutureBuilder(
+      future: Hive.openBox("tasks"),
+      //snapshot contains information about the status of the future and the value that was obtained from the future
+      builder: (BuildContext _context, AsyncSnapshot _snapshot) {
+        if (_snapshot.connectionState == ConnectionState.done) {
+          //whether the snapshot's future is done or not
+          
+          return _tasksList(); //return the list to re-render
+        } else {
+          //Center(child: ) widget centers the child widget it has
+          return const Center(
+              child:
+                  CircularProgressIndicator()); //render a loading circular indicator
+        }
+      },
     );
   }
 
@@ -68,11 +98,37 @@ class _HomePageState extends State<HomePage> {
 
   Widget _addTaskButton() {
     //button for adding a task in our list
-    return FloatingActionButton(onPressed: () {  //by default it appears on right-bottom corner and stays there irrespective of if we scroll
-      //what happens when we click on button
-      
-    },
-    child: const Icon(Icons.add), //'+' icon added
+    //by default it appears on right-bottom corner and stays there irrespective of if we scroll
+    return FloatingActionButton(
+      onPressed: _displayTaskPopup, //what happens when we click on button,
+      //passing the function that we created
+
+      child: const Icon(Icons.add), //'+' icon added
+    );
+  }
+
+  void _displayTaskPopup() {
+    //kind of like a toast
+    showDialog(
+      context: context,
+      builder: (BuildContext _context) {
+        //builder is responsible for rendering the dialog
+        return AlertDialog(
+          //widget that alerts the user
+          title: const Text("Add new task!"),
+          content: TextField(
+            //where the user can input a text
+            onSubmitted:
+                (_value) {}, //when we submit (the _value is a string parameter)
+            onChanged: (_value) {
+              setState(() {
+                //if we don't use setState function , the variable _newTaskContent would still be set to _value , but it won't re-render the app and our changes wouldn't be visible on the screen
+                _newTaskContent = _value;
+              });
+            }, //what happens when we start typing
+          ),
+        );
+      },
     );
   }
 }
